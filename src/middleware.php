fef9ea1c208eb44d800b295a88c75fa86a99a7e7
hirsch88 <gery.hirschfeld@w3tec.ch>
2016-06-01 19:17:29 +0200

    adds cars middleware

--- src/middleware.php
+++ src/middleware.php
@@ -1,3 +1,13 @@
 <?php
 
 //// Application middleware
+$corsOptions = array(
+  'origin' => '*',
+  'exposeHeaders' => array('Access-Control-Allow-Headers', 'Authorization', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept'),
+  'maxAge' => 1728000,
+  'allowCredentials' => True,
+  'allowMethods' => array('GET', 'POST', 'OPTIONS', 'DELETE', 'PUT'),
+  'allowHeaders' => array('Access-Control-Allow-Headers', 'Authorization', 'Origin', 'X-Requested-With', 'Content-Type', 'Accept')
+);
+$cors = new \CorsSlim\CorsSlim($corsOptions);
+$app->add($cors);
